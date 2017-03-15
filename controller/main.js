f7c9c5f6a9e5c246dd623966b75390e478000d17
Surya Vaigundth M R <surya.vaigundth@in.ibm.com>
2017-03-15 19:11:08 +0530

    Added incident description card in the popup

--- controller/main.js
+++ controller/main.js
@@ -109,6 +109,7 @@ app.controller('chatWindow', ['$scope', 'DataStream', function ($scope, DataStre
         '<script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.4.8/angular-sanitize.js"></script>' +
         '<script>if (window.module) module = window.module;</script>' +
         '<script>function clickNext() {var nextValue = 0;var counter = parseInt(document.getElementById(\'counter\').value);var totalPages = parseInt(document.getElementById(\'finalCounter\').value);var finalCounter = totalPages - 1;var id = "content" + counter;if (counter == finalCounter) {counter = -1;}nextValue = counter + 1;document.getElementById(\'counter\').value = nextValue;document.getElementById(id).style.display = \'none\';document.getElementById("content" + nextValue).style.display = \'block\';document.getElementById("pageNumber").innerHTML = (parseInt(nextValue)+1) +"/"+ totalPages;}function clickPrevious() {var pastValue = 0;var counter = parseInt(document.getElementById(\'counter\').value);var totalPages = parseInt(document.getElementById(\'finalCounter\').value);var finalCounter = totalPages - 1;var id = "content" + counter;if (counter == 0) {counter = finalCounter + 1;}pastValue = counter - 1;document.getElementById(\'counter\').value = pastValue;document.getElementById(id).style.display = \'none\';document.getElementById("content" + pastValue).style.display = \'block\';document.getElementById("pageNumber").innerHTML = (parseInt(pastValue)+1)+"/"+ totalPages;}</script>' +
+        '<script>function showDetails(incidentId) {document.getElementById(\'incidentContainer\').style.display = "none";document.getElementById(incidentId + \'container\').style.display = "block";}function goBack(incidentId) {document.getElementById(\'incidentContainer\').style.display = "block";document.getElementById(incidentId + \'container\').style.display = "none";}</script>' +
         '<script>const remote = require(\'electron\').remote;function closeWindow(){var window = remote.getCurrentWindow();window.close();}</script>';
 
     let style = '<style>' +
@@ -154,7 +155,48 @@ app.controller('chatWindow', ['$scope', 'DataStream', function ($scope, DataStre
         '   -webkit-app-region: drag;' +
         '    height: 22px;} ' +
         'img{' +
-        '   max-width: 650px}' +
+        '   max-width: 650px} ' +
+        '#backButton {  ' +
+        '   width: 42px;  ' +
+        '   height: 42px;  }  ' +
+        '.backImg {  ' +
+        '   width: 28px;  ' +
+        '   height: 28px;  ' +
+        '   margin-top: 15px;  ' +
+        '   border-radius: 50%/50%;  ' +
+        '   background-color: white;  ' +
+        '   -moz-transform: scaleX(-1); ' +
+        '   -o-transform: scaleX(-1);  ' +
+        '   -webkit-transform: scaleX(-1);  ' +
+        '   transform: scaleX(-1);  ' +
+        '   filter: FlipH;  ' +
+        '   -ms-filter: "FlipH";  ' +
+        '   cursor: pointer;  ' +
+        '   left: -20px;  }  ' +
+        '.backImg:hover, .editImg:hover {  ' +
+        '   width: 42px;  ' +
+        '   height: 42px;  ' +
+        '   box-shadow: 0 0 10px rgba(0, 0, 0, 0.6);  ' +
+        '   -moz-box-shadow: 0 0 10px rgba(0, 0, 0, 0.6);  ' +
+        '   -webkit-box-shadow: 0 0 10px rgba(153, 248, 255, 0.6); ' +
+        '   -o-box-shadow: 0 0 10px rgba(153, 248, 255, 0.6);  }  ' +
+        '.editImg {  ' +
+        '   width: 28px; ' +
+        '   height: 28px;  ' +
+        '   margin-top: 15px;  ' +
+        '   border-radius: 50%/50%; ' +
+        '   background-color: white;  ' +
+        '   cursor: pointer;  }  ' +
+        '.incDetContainer {  ' +
+        '   box-shadow: 0 0 10px rgba(0, 0, 0, 0.6);' +
+        '   -moz-box-shadow: 0 0 10px rgba(0, 0, 0, 0.6)); ' +
+        '   -webkit-box-shadow: 0 0 10px rgba(0, 0, 0, 0.6)); ' +
+        '   -o-box-shadow: 0 0 10px rgba(0, 0, 0, 0.6));' +
+        '   padding-left: 10px; ' +
+        '   padding-bottom: 10px; ' +
+        '   border-radius: 10px;  } ' +
+        '.navigation {  ' +
+        '   height: 40px;  }' +
         '</style>';
 
     //To set up the Pop up HTML for Discovery service data
@@ -179,17 +221,27 @@ app.controller('chatWindow', ['$scope', 'DataStream', function ($scope, DataStre
     //To set up the Pop up HTML for Cloudant data
     const makeIncidentTable = function (data) {
         let html = headers + scripts + style + '</head><body>' +
-            '<div id="readingPane" ng-app="popup"><div id="close" style="float: right; margin-right: 5px; height: 20px;"><button type="button" class="close" aria-label="Close" onclick="closeWindow()">' +
+            '<div id="readingPane" ng-app="popup"><div class="heading"></div><div id="close" style="float: right; margin-right: 5px; height: 20px;"><button type="button" class="close" aria-label="Close" onclick="closeWindow()">' +
             '<span aria-hidden="true">&times;</span></button></div>' +
             '<div class="contentRead" id="content0">' +
-            '<div class="container"><h2>Incidents</h2>' +
+            '<div class="container" id="incidentContainer" style="display:block"><h2>Incidents</h2>' +
             '<div class="table-responsive">' +
             '<table class="table"><thead><tr><th>#</th><th>Priority</th><th>Description</th></tr></thead><tbody>';
-
+        let incidentCardHtml = '';
         data.forEach(d => {
-            html = html + '<tr><td>' + d.incidentRef + '</td><td>' + d.priority + '</td><td>' + truncate(d.incidentDesc) + '</td></tr>';
+            html = html + '<tr><td class="incident" style="cursor:pointer" onclick="showDetails(\''+d.incidentRef+'\')">' + d.incidentRef + '</td><td>' + d.priority + '</td><td>' + truncate(d.incidentDesc) + '</td></tr>';
+            incidentCardHtml = incidentCardHtml + '<div id="'+d.incidentRef+'container" class="incDetContainer" style="display:none">' +
+                '<div class="navigation">' +
+                '<div id="backButton" onclick="goBack(\''+d.incidentRef+'\')" style="marign:0;padding:0; float:left;"><img src="http://icons.iconarchive.com/icons/icons8/ios7/512/Arrows-Right-icon.png" class="backImg" alt="Back"></div>' +
+                '<div id="editButton" style="padding:0;float:right;margin-right:10px;"><img src="https://cdn3.iconfinder.com/data/icons/google-material-design-icons/48/ic_mode_edit_48px-128.png" class="editImg" alt="Edit"></div></div>' +
+                '<div class="card" style="width: 50rem;margin-left:20px;margin-right:20px;"><div class="card-block">' +
+                '<h2 class="card-title">'+d.incidentRef+'</h2>' +
+                '<p class="card-text priority"><b>Priority:</b><br/>'+d.priority+'</p>' +
+                '<p class="card-text description"><b>Description:</b><br/>'+d.incidentDesc+'</p>' +
+                '</div></div></div>';
         });
-        return html + '</tbody></table></div></div></div></div></body></html>';
+        return html + '</tbody></table></div></div>' + incidentCardHtml +
+            '</div></div></body></html>';
     };
 
     /*This method inserts ellipsis if the text length is more than 68*/
@@ -211,10 +263,9 @@ app.controller('chatWindow', ['$scope', 'DataStream', function ($scope, DataStre
             DataStream.context = data.context;
         }
         if (data.type === 'discovery') {
-
             res.forEach((r,i) => {
                 resArray.push(
-                    '<p><b>Here is the '+ numbers[i+1]+'best answer for: '+ data.question +'</b></p><p align="center"><b>' + r['Item Name'] + '</b></p>' + r['Documentation with HTML'].replace(/\\/g, "\\\\")
+                    '<p><b>Here is the '+ numbers[i+1]+'best answer for: <i>'+ data.question +'</i></b></p><p align="center"><b>' + r['Item Name'] + '</b></p>' + r['Documentation with HTML'].replace(/\\/g, "\\\\")
                         .replace(/\$/g, "\\$")
                         .replace(/'/g, "\\'")
                         .replace(/"/g, "\\\""));
